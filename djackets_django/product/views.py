7f6dd018a9a50784c3cec6e8bf46e044f8ec5d43
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-11-25 23:58:19 +0900

    Modify Linting

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,6 +1,5 @@
 from django.http import Http404
 
-from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView, RetrieveAPIView
 from rest_framework.response import Response
 
